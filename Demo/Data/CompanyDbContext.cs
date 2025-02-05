7f16de7d71d011e5da64ed1b6969aceba125f3cc
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-02-05 13:00:14 +0200

    Add project files.

---
+++ Demo/Data/CompanyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data
+{
+    internal class CompanyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=DemoEf02;Trusted_Connection=True;Trustedservercertificate=True");
+        }
+    }
+}
