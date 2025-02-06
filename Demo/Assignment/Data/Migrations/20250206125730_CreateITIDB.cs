15125a2f4246649826d1dbd71ec9c7cc6038b2cb
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-02-06 15:04:39 +0200

    Create - ITIDatabase

---
+++ Demo/Assignment/Data/Migrations/20250206125730_CreateITIDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Assignment.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateITIDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
